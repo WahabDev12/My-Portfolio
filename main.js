89e9bceb4e8ed6bba850d4b97ee3561d9e33846d
Abass Abdul Wahab <70736338+WahabDev12@users.noreply.github.com>
2021-03-06 15:06:34 +0000

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ window.addEventListener('scroll', function(){
       menuToggle.classList.toggle('active');
       menu.classList.toggle('active');
     } 
+
+
+    document.querySelector("form").addEventListener("submit", handleSubmit);
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const name = document.getElementById("name");
+      const email = document.getElementById("email");
+      const message = document.getElementById("message");
+    //   let myForm = document.getElementById('name');
+      let formData = new FormData(name,email,message)
+      fetch('/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString()
+      }).then(() => console.log('Form successfully submitted')).catch((error) =>
+        alert(error))
+    }
